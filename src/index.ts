cc0496d8ae64379c7f2467753ffcd42b5ea853f0
Elvin Aghalarov <aghalarovelvin@gmail.com>
2023-06-16 20:46:49 +0300

    ts refactor for rabbitmq

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,58 @@
-import dotenv from "dotenv";
-import { wss } from "./ws";
+import { Channel, Connection, Message, connect } from "amqplib/callback_api";
 import WebSocket from "ws";
-import { getData } from "./responses";
-import { connect } from "amqplib/callback_api";
+import dotenv from "dotenv";
+
 import { RABBITMQ } from "./config/app.config";
+import { getData } from "./responses";
+import { wss } from "./ws";
 
 dotenv.config();
 
 wss.on("connection", function connection(ws: WebSocket) {
   // rabbitMQ connection
-  connect(RABBITMQ.connectionString, function (error0: any, connection: any) {
-    if (error0) {
-      throw error0;
-    }
-    connection.createChannel(function (error1: any, channel: any) {
-      if (error1) {
-        throw error1;
+  connect(
+    RABBITMQ.connectionString,
+    function (error0: any, connection: Connection) {
+      if (error0) {
+        throw error0;
       }
-      const queue = "test-queue";
-
-      channel.assertQueue(queue, {
-        durable: true,
-      });
-
-      //  behaviour on Websocket
-      console.log(" [*] Waiting for messages in %s from websocket.");
-      ws.on("message", async function message(data: WebSocket.RawData) {
-        try {
-          ws.send(JSON.stringify(await getData(data)));
-        } catch (e: any) {
-          ws.send(e?.message || "Undefined error occured!");
+      connection.createChannel(function (error1: any, channel: Channel) {
+        if (error1) {
+          throw error1;
         }
-      });
+        const queue = "test-queue";
 
-      // behaviour on rabbitMQ
-      console.log(" [*] Waiting for messages in %s from rabbit.", queue);
-      channel.consume(
-        queue,
-        function (msg: any) {
-          const msgStr = msg?.content?.toString();
-          console.log(" [x] Received %s", msgStr);
-          ws.send(` [x] Received ${msgStr}`);
-        },
-        {
-          noAck: true,
-        }
-      );
-    });
-  });
+        channel.assertQueue(queue, {
+          durable: true,
+        });
+
+        //  behaviour on Websocket
+        console.log("[*] Waiting for messages in %s from websocket.");
+        ws.on("message", async function message(data: WebSocket.RawData) {
+          try {
+            ws.send(JSON.stringify(await getData(data)));
+          } catch (e: any) {
+            ws.send(e?.message || "[x] Undefined error occured!");
+          }
+        });
+
+        // behaviour on rabbitMQ
+        console.log("[*] Waiting for messages in %s from rabbitMQ.", queue);
+        channel.consume(
+          queue,
+          function (msg: Message | null) {
+            const msgStr =
+              msg?.content?.toString() || "[x] Undefined message from rabbitMQ";
+            console.log("[x] Received %s", msgStr);
+            ws.send(`[x] Received ${msgStr}`);
+          },
+          {
+            noAck: true, // not sending any acknowledgement back to queue
+          }
+        );
+      });
+    }
+  );
 
   ws.send("Hi from Chatman!");
 });
